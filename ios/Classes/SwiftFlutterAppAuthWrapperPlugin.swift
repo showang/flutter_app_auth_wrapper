e6bb1d2268b5ec4a75e6acc3080b3262ca5d7c8c
zonble <zonble@gmail.com>
2019-06-11 12:04:15 +0800

    Fixes a crash that is due to that AppAuth may return expiry date in a wrong type.

--- ios/Classes/SwiftFlutterAppAuthWrapperPlugin.swift
+++ ios/Classes/SwiftFlutterAppAuthWrapperPlugin.swift
@@ -3,64 +3,61 @@ import UIKit
 import AppAuth
 
 public class SwiftFlutterAppAuthWrapperPlugin: NSObject, FlutterPlugin {
-	
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_app_auth_wrapper", binaryMessenger: registrar.messenger())
-	let eventChannel = FlutterEventChannel(name: "oauth_completion_events", binaryMessenger: registrar.messenger())
-	
-	let currentVC = (registrar.messenger() as? FlutterEngine)?.viewController
-	
-	let instance = SwiftFlutterAppAuthWrapperPlugin(with: currentVC)
-	
-	
-	eventChannel.setStreamHandler(instance)
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-	
-	private var viewController :UIViewController?
-	
+
+	public static func register(with registrar: FlutterPluginRegistrar) {
+		let channel = FlutterMethodChannel(name: "flutter_app_auth_wrapper", binaryMessenger: registrar.messenger())
+		let eventChannel = FlutterEventChannel(name: "oauth_completion_events", binaryMessenger: registrar.messenger())
+		let currentVC = (registrar.messenger() as? FlutterEngine)?.viewController
+		let instance = SwiftFlutterAppAuthWrapperPlugin(with: currentVC)
+
+		eventChannel.setStreamHandler(instance)
+		registrar.addMethodCallDelegate(instance, channel: channel)
+	}
+
+	private var viewController: UIViewController?
+
 	init(with vc: UIViewController?) {
 		super.init()
 		self.viewController = vc
 	}
-	
+
 	var eventSink: FlutterEventSink?
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-	switch(call.method){
-	case "startOAuth":
-		print("on startOAuth")
-		guard let json = call.arguments as? String,
-			let authConfig = AuthConfig(json) else {
+	public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+		switch (call.method) {
+		case "startOAuth":
+			print("on startOAuth")
+			guard let json = call.arguments as? String,
+			      let authConfig = AuthConfig(json) else {
 				handle(error: FlutterAppAuthWrapperError.invalidParameters)
 				result(true)
 				return
-		}
-		startOAuthBy(result, authConfig: authConfig)
-	default: break
+			}
+			startOAuthBy(result, authConfig: authConfig)
+		default: break
 //		result
+		}
+
 	}
-	
-  }
-	
+
 	var authRequest: OIDAuthorizationRequest?
-	public static var authFlow:OIDExternalUserAgentSession?
-	
+	public static var authFlow: OIDExternalUserAgentSession?
+
 	func startOAuthBy(_ result: FlutterResult, authConfig: AuthConfig) {
 		guard let authEndpoint = URL(string: authConfig.endpoint.auth),
-			let tokenEndpoint = URL(string: authConfig.endpoint.token),
-			let redirectURL = URL(string: authConfig.redirectURL) else {
-				handle(error: FlutterAppAuthWrapperError.invalidURLs)
-				result(true)
-				return
+		      let tokenEndpoint = URL(string: authConfig.endpoint.token),
+		      let redirectURL = URL(string: authConfig.redirectURL) else {
+			handle(error: FlutterAppAuthWrapperError.invalidURLs)
+			result(true)
+			return
 		}
-		
+
 		guard let vc = UIApplication.shared.keyWindow?.rootViewController else {
 			handle(error: FlutterAppAuthWrapperError.noViewController)
 			result(true)
 			return
 		}
-		
+
 		let config = OIDServiceConfiguration(authorizationEndpoint: authEndpoint, tokenEndpoint: tokenEndpoint)
 		var additionalParameters = [String: String]()
 		if authConfig.prompt.count > 0 {
@@ -75,23 +72,25 @@ public class SwiftFlutterAppAuthWrapperPlugin: NSObject, FlutterPlugin {
 				return current
 			}
 		}
-		
+
 		authRequest = OIDAuthorizationRequest(configuration: config,
-											  clientId: authConfig.clientID,
-											  clientSecret: authConfig.clientSecret,
-											  scopes: authConfig.scopes ?? [],
-											  redirectURL: redirectURL,
-											  responseType: OIDResponseTypeCode,
-											  additionalParameters: additionalParameters)
+				clientId: authConfig.clientID,
+				clientSecret: authConfig.clientSecret,
+				scopes: authConfig.scopes ?? [],
+				redirectURL: redirectURL,
+				responseType: OIDResponseTypeCode,
+				additionalParameters: additionalParameters)
 		SwiftFlutterAppAuthWrapperPlugin.authFlow = OIDAuthState.authState(byPresenting: authRequest!, presenting: vc) { authState, error in
 			print("auth callback")
 			if let authState = authState,
-				let response = authState.lastTokenResponse {
+			   let response = authState.lastTokenResponse {
 				print("auth state: \(String(describing: authState))")
 				var dict: [AnyHashable: Any] = [:]
 				dict["access_token"] = response.accessToken ?? ""
-				dict["expires_at"] = Int((response.accessTokenExpirationDate?.timeIntervalSince1970 ?? 0) * 1000)
 				dict["refresh_token"] = response.refreshToken ?? ""
+				// The Objective-C code of AppAuth library may return an object in a type other than NSDate.
+				// Thus, we should not be naive to trust the type here.
+				dict["expires_at"] = Int(((response.accessTokenExpirationDate as? Date)?.timeIntervalSince1970 ?? 0) * 1000)
 				do {
 					let data = try JSONSerialization.data(withJSONObject: dict, options: JSONSerialization.WritingOptions.init(rawValue: 0))
 					let string = String(data: data, encoding: .utf8)
@@ -107,7 +106,7 @@ public class SwiftFlutterAppAuthWrapperPlugin: NSObject, FlutterPlugin {
 		}
 		result(true)
 	}
-	
+
 	func handle(error: Error) {
 		let flutterError = FlutterError(code: "OAuthError", message: error.localizedDescription, details: "No details")
 		eventSink?(flutterError)
@@ -123,7 +122,7 @@ public enum FlutterAppAuthWrapperError: Error, LocalizedError {
 	case invalidURLs
 	/// No view controller to present the web view to authenticate users.
 	case noViewController
-	
+
 	public var errorDescription: String? {
 		switch self {
 		case .invalidParameters:
@@ -138,12 +137,12 @@ public enum FlutterAppAuthWrapperError: Error, LocalizedError {
 
 
 extension SwiftFlutterAppAuthWrapperPlugin: FlutterStreamHandler {
-	
+
 	public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
 		self.eventSink = events
 		return nil
 	}
-	
+
 	public func onCancel(withArguments arguments: Any?) -> FlutterError? {
 		self.eventSink = nil
 		return nil
